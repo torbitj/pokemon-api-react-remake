b7856ec59141e8ad2c1398af5349c47519e64bad
JT <jstorbit@gmail.com>
2025-12-17 09:41:45 -0700

    Add StarterList component render based on region selected

--- src/StarterList.jsx
+++ src/StarterList.jsx
@@ -1,9 +1,31 @@
 import { usePokedex } from "./PokedexContext";
+import StarterListItem from "./StarterListItem"
 
 const StarterList = () => {
-  const { starters } = usePokedex();
+  const { grassType, fireType, waterType, region } = usePokedex();
+  console.log(region);
   return (
-    
+    <>
+      <h1>{region.name} Starter Pokémon</h1>
+      <figure>
+        <h2>Grass</h2>
+        <ul id="grass">
+          {grassType.map((starter) => <StarterListItem key={starter.url} starter={starter} />)}
+        </ul>
+      </figure>
+      <figure>
+        <h2>Water</h2>
+        <ul id="fire">
+          {fireType.map((starter) => <StarterListItem key={starter.url} starter={starter} />)}
+        </ul>
+      </figure>
+      <figure>
+        <h2>Fire</h2>
+        <ul id="water">
+          {waterType.map((starter) => <StarterListItem key={starter.url} starter={starter} />)}
+        </ul>
+      </figure>
+    </>
   )
 }
 
